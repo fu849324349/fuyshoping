a55ed328e35c10170440c5351bdbed7ec6ed4d50
Administrator <849324349@qq.com>
2019-01-04 21:03:48 +0800

    first commit

---
+++ src/main/java/com/neuedu/FuyshopingApplication.java
@@ -0,0 +1,14 @@
+package com.neuedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuyshopingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuyshopingApplication.class, args);
+    }
+
+}
+
